a3bca72566caa5b63efa572c854d8372f5e532b1
Leirannt <93717185+Leirannt@users.noreply.github.com>
2023-04-02 19:20:01 +0700

    Add project files.

---
+++ tpmodul7_kelompok_10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodul7_kelompok_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
